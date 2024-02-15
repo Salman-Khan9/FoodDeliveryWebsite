116414a482f87a3c44c6bb12f32291d53b2ac0a3
salman karim <124361112+Salman-Khan9@users.noreply.github.com>
2024-02-15 17:08:43 +0500

    food-web
    food-web

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -4,6 +4,8 @@ import { Link } from "react-router-dom";
 import { selectitems } from "../../Redux/Slices/ItemsSlice";
 import Modal from "../../Modal/Modal";
 import Cart from "../../Screens/Cart/Cart";
+import { MdAddShoppingCart } from "react-icons/md";
+import { Badge } from "react-bootstrap";
 
 const Navbar = () => {
   const data = useSelector(selectitems)
@@ -47,7 +49,7 @@ const Navbar = () => {
                 Add-Food-Item
               </Link>
             </li>
-            <div className="btn text-white mx-2" onClick={()=>setcartview(true)}>Cart { data? data.length-1:0}
+            <div className="btn text-white  " onClick={()=>setcartview(true)}><MdAddShoppingCart />Cart <Badge pill bg="danger">{ data? data.length-1:0}</Badge>
             </div>
             {cartview?<Modal onClose={()=>setcartview(false)}  ><Cart/></Modal>:null}
           </ul>
