4bcfd7c72b31bb70cd030358514965701b756063
salman karim <124361112+Salman-Khan9@users.noreply.github.com>
2024-02-23 21:54:23 +0500

    cors
    cors

--- backend/server.js
+++ backend/server.js
@@ -12,12 +12,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
 
-const allowedOrigins = ['https://food-delivery-website-frontend.vercel.app'];
-app.use(cors({
-  origin : ['https://food-delivery-website-frontend.vercel.app'],
-  allowedHeaders:true,
-  credentials: true // Allow credentials
-}));
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://food-delivery-website-frontend.vercel.app');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 app.use(bodyParser.json());
 
 app.use(route);
