f2e9f0d35e426ebeb69cb24d0d9acfc7820394b4
salman karim <124361112+Salman-Khan9@users.noreply.github.com>
2024-02-23 19:42:17 +0500

    backend : cors origin
    backend : cors origin

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.use(cookieParser())
 
 app.use(
   cors({
-    origin: ["https://food-delivery-website-frontend.vercel.app/"],
+    origin: ["https://food-delivery-website-frontend.vercel.app"],
     credentials:true
   })
 );
