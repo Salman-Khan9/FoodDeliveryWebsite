709b43a12663c538739304fea83ea0851eeaffff
salman karim <124361112+Salman-Khan9@users.noreply.github.com>
2024-02-27 17:08:56 +0500

    post
    post

--- backend/Routes/UserRoutes.js
+++ backend/Routes/UserRoutes.js
@@ -75,10 +75,10 @@ secure : true,
   }
 })
 
-route.get("/logout",async(req,res)=>{
+route.post("/logout",async(req,res)=>{
 
    try {
-res.clearCookie('token', {path:'/',domain:'.food-delivery-website-bay.vercel.app'})
+res.clearCookie('token', {path:'/',domain:'food-delivery-website-bay.vercel.app'})
   //res.cookie('token', "", { expires: Date.now(),path:"/",secure:true,sameSite:"none" ,domain:"food-delivery-website-bay.vercel.app" });
    // res.cookie("token",token,{
      // path : "/",
