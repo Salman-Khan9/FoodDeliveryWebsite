ce94f622ca2b848abb3622d72d7287806d75539a
salman karim <124361112+Salman-Khan9@users.noreply.github.com>
2024-02-27 16:34:05 +0500

    new domain
     new domain

--- backend/Routes/UserRoutes.js
+++ backend/Routes/UserRoutes.js
@@ -78,7 +78,8 @@ secure : true,
 route.get("/logout",async(req,res)=>{
 
    try {
-  res.cookie('token', "", { expires: Date.now(),path:"/",secure:true,sameSite:"none" ,domain:"food-delivery-website-frontend.vercel.app" });
+    
+  res.cookie('token', "", { expires: Date.now(),path:"/",secure:true,sameSite:"none" ,domain:"https://food-delivery-website-bay.vercel.app/" });
    // res.cookie("token",token,{
      // path : "/",
       //httpOnly:true,
