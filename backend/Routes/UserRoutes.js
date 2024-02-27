466483294a3089e0298cdab503f9ce04b2b5f77a
salman karim <124361112+Salman-Khan9@users.noreply.github.com>
2024-02-27 16:26:09 +0500

    set date
    SET DATE

--- backend/Routes/UserRoutes.js
+++ backend/Routes/UserRoutes.js
@@ -78,7 +78,7 @@ secure : true,
 route.get("/logout",async(req,res)=>{
 
    try {
-  res.cookie('token', "", { expires: new Date(Date.now()-86400*1000),path:"/",secure:true,sameSite:"none" ,domain:"food-delivery-website-frontend.vercel.app" });
+  res.cookie('token', "", { expires: Date.now(),path:"/",secure:true,sameSite:"none" ,domain:"food-delivery-website-frontend.vercel.app" });
    // res.cookie("token",token,{
      // path : "/",
       //httpOnly:true,
